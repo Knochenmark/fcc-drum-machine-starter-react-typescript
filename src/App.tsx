5a2d4c69d53315c8026db4aa3c9ea8c83993c0a1
knochenmark <marktodd.hl@gmail.com>
2019-01-06 14:36:06 +0100

    Add drummachine to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+import React, { Component } from 'react';
+
+import { drumPadInfoList } from './_domain/drum-pad-list';
+import DrumMachine from './components/drum-machine';
+
+export default class App extends Component {
+  render(): JSX.Element {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <DrumMachine drumPadInfoList={drumPadInfoList} />
     );
   }
 }
-
-export default App;
